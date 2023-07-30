2e9a988f67db39f14350bf2d17e419665418f9ac
Mohamed Taha <thm614372@gmail.com>
2023-07-30 11:19:54 +0300

    Solve digit problem

--- src/problemSolving/digit.java
+++ src/problemSolving/digit.java
@@ -4,10 +4,23 @@
  */
 package problemSolving;
 
+import java.util.Scanner;
+
 /**
  *
  * @author khale
  */
 public class digit {
-    
+
+    public static void main(String[] args) {
+        Scanner M = new Scanner(System.in);
+
+        int num = M.nextInt();
+        int k = M.nextInt();
+
+        for (int i = 1; i < k; i++) {
+            num /= 10;
+        }
+        System.out.println(num % 10);
+    }
 }
