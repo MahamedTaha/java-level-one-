3e7160bc6bd2e775ca3577adf0b75ce299d011dc
Mohamed Taha <thm614372@gmail.com>
2023-08-06 21:00:46 +0300

    improve the readablity of the code

---
+++ src/problemSolving/electricalResistors.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemSolving;
+
+import java.util.Scanner;
+
+public class electricalResistors {
+
+    public static void main(String[] args) {
+        Scanner inpute = new Scanner(System.in);
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+}
--- src/problemSolving/pythagoreanTheorem.java
+++ src/problemSolving/pythagoreanTheorem.java
@@ -12,25 +12,21 @@ public class pythagoreanTheorem {
         double num1 = inpute.nextDouble();
         System.out.print("Enter number two : ");
         double num2 = inpute.nextDouble();
-        if (num1 >= num2) {
-
-            int pow1 = 2;
-            double powernum1 = Math.pow(num1, pow1);
-
-            int pow2 = 2;
-            double powernum2 = Math.pow(num2, pow2);
-
-            double a = powernum1 - powernum2;
-            double b = 2 * num1 * num2;
-            double c = Math.sqrt(powernum1 + powernum2);
-
-            System.out.println("The result of subtracting the square of the two numbers : " + a);
-            System.out.println("Product of 2 * first number * second number : " + b);
-            System.out.println("The square root is : " + c);
-
-        } else {
+        if (num1 < num2) {
             System.out.println("The first number is smaller than the second number");
+            return;
         }
+
+        double powernum1 = Math.pow(num1, 2);
+        double powernum2 = Math.pow(num2, 2);
+
+        double a = powernum1 - powernum2;
+        double b = 2 * num1 * num2;
+        double c = Math.sqrt(powernum1 + powernum2);
+
+        System.out.println("The result of subtracting the square of the two numbers : " + a);
+        System.out.println("Product of 2 * first number * second number : " + b);
+        System.out.println("The square root is : " + c);
 
     }
 
