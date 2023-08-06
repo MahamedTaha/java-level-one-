709cdc65be68d8780b611ea5a13564edac203fb0
Mohamed Taha <thm614372@gmail.com>
2023-08-07 00:10:26 +0300

    [200~3- Resistance (simplified)

--- src/problemSolving/electricalResistors.java
+++ src/problemSolving/electricalResistors.java
@@ -11,23 +11,16 @@ public class electricalResistors {
     public static void main(String[] args) {
         Scanner inpute = new Scanner(System.in);
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        System.out.print("Enter The First Resistance : ");
+        double FirstResistance = inpute.nextDouble();
+        System.out.print("Enter The Scound Resistance : ");
+        double secondResistance = inpute.nextDouble();
+
+        double straightResistance = FirstResistance + secondResistance;
+        double parallelResistance = (1 / FirstResistance) + (1 / secondResistance);
+        System.out.println("the result straight Resistance : " + straightResistance);
+        System.out.println("the result parallel Resistance : " + parallelResistance);
+
     }
 
 }
