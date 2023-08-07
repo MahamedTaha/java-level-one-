b8e6ad7d6d96cf7ed9670a7ce4d2ad9df60c4aa8
Mohamed Taha <thm614372@gmail.com>
2023-08-07 21:24:56 +0300

    Strings

---
+++ src/problemSolving/strings.java
@@ -0,0 +1,25 @@
+package problemSolving;
+
+/*
+Strings
+Write a single write statement that prints:
+somewhere over / the vast ocean \ 'there must' be a land 
+where my thoughts / will meat the distant shore ..."
+complained a "lonely and longing" singer 
+*/
+  
+import java.util.Scanner;
+public class strings {
+    public static void main(String[] args) {
+        Scanner inpute = new Scanner (System.in);
+        
+        
+        System.out.println("somewhere over / the vast ocean \\ 'there must' be a land \nwhere my thoughts / will meat the distant shore ...\"\ncomplained a \"lonely and longing\" singer ");
+        
+        
+        
+        
+         
+    }
+    
+}
