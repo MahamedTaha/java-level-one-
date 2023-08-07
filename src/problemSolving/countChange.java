684fcb095a63578160cc9cc794386f4b968fe59e
Mohamed Taha <thm614372@gmail.com>
2023-08-07 19:50:05 +0200

    Solve Count Change problem

--- src/problemSolving/countChange.java
+++ src/problemSolving/countChange.java
@@ -1,8 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package problemSolving;
+
+/*
+- Count Change
+Write a Java program CountChange to count change. Given the number of quarter, dimes,
+nickels and cents the program should output the total as a single value in dollars and cents.
+Hint:
+● One dollar corresponds to 100 cents
+● One quarter corresponds to 25 cents
+● One dime corresponds to 10 cents
+● One nickel corresponds to 5 cents
+For example if we have: 3 quarters, 2 dimes, 1 nickel and 6 cents, the total is 1.06 dollars.
+*/
 
 import java.util.Scanner;
 
@@ -10,35 +18,32 @@ public class countChange {
 
     public static void main(String[] args) {
         Scanner inpute = new Scanner(System.in);
-        
-        double cent = 1;
-        double OneDollar = 100 * cent ;
-        double OneQuarter = 25 * cent;
-        double OneDime = 10 * cent;
-        double OneNickel = 5 * cent ;
-        
+
         System.out.print("Enter the number of dollars : ");
-        double a = inpute.nextDouble();
+        double dollars = inpute.nextDouble();
+
         System.out.print("Enter the number of quarters : ");
-        double b = inpute.nextDouble();
+        double quarters = inpute.nextDouble();
+
         System.out.print("enter the number of dimes : ");
-        double c = inpute.nextDouble();
+        double dimes = inpute.nextDouble();
+
         System.out.print("Enter the number of nickels : ");
-        double d = inpute.nextDouble();
+        double nickels = inpute.nextDouble();
+
         System.out.print("Enter the number of cents : ");
-        double e = inpute.nextDouble();
-        
-        double theNumberOfDollars = a * OneDollar;
-        double theNumberOfQuarters = b * OneQuarter; 
-        double theNumberOfDimes = c * OneDime;
-        double theNumberOfNickels = d * OneNickel; 
-        double theNumberOfCents = e * cent;
-        
+        double cents = inpute.nextDouble();
+
+        double theNumberOfDollars = dollars * 100;  // OneDollar = 100 * cent
+        double theNumberOfQuarters = quarters * 25; // OneQuarter = 25 * cent
+        double theNumberOfDimes = dimes * 10;   // OneDime = 10 * cent
+        double theNumberOfNickels = nickels * 5; // OneNickel = 5 * cent
+        double theNumberOfCents = cents * 1;  // cent = 1
+
         double result = (theNumberOfDollars + theNumberOfQuarters + theNumberOfDimes + theNumberOfNickels + theNumberOfCents) / 100;
-        
+
         System.out.println("the number of dollar : " + result + " Dollar");
-        
-        
+
     }
 
 }
