8ddad90b0c004aa1d4ee7c7e82b730b355e2fed2
Mohamed Taha <thm614372@gmail.com>
2023-08-22 20:22:22 +0300

    Reverse Array

--- src/problemSolving/ReverseInput.java
+++ src/problemSolving/ReverseInput.java
@@ -8,6 +8,33 @@ package problemSolving;
  *
  * @author khale
  */
+import java.util.Scanner;
+
 public class ReverseInput {
-    
+
+    public static void main(String[] args) {
+        Scanner inpute = new Scanner(System.in);
+
+        int size = inpute.nextInt();
+        inpute.nextLine();
+        char[] x = new char[size];
+        char[] y = new char[size];
+
+        for (int i = 0; i < size; i++) {
+
+            String str = inpute.nextLine();
+            x[i] = str.charAt(0);
+
+        }
+
+        for (int i = size - 1, m = 0; i >= 0; i--, m++) {
+            y[m] = x[i];
+
+        }
+
+        for (int i = 0; i < size; i++) {
+            System.out.println(y[i]);
+        }
+
+    }
 }
